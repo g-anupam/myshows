6f912cce728d4bf008974352ea54340a17ff6b50
AyushChakraborty <chakrabortyayush83@gmail.com>
2024-10-27 23:59:40 +0530

    Feat: Update functionality of site added About component which builds up some of the components of the landing page of the site

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# myshows
-The 2024 Sem 3 Web Tech Project for booking movie tickets.
+# React + Vite
 
-Instructions to run the code :
-```sh
-cd frontend/
-npm run dev
-```
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/About.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './About.css'
+import { link } from 'fs'
+
+function About() {
+    const [linkDisabled, setLinkDisabled] = useState(true)
+
+    const clickFunction = () => {
+        var options = document.querySelector("#options")
+        var moreButton = document.querySelector("#b1")
+        options.style.opacity = options.style.opacity === '1' ? '0' : '1' 
+    }
+ 
+    return ( //the href thing is done so that once the fields are not visible, all their links 
+        //become undefined effectively making it useless, and the fields do point to some other
+        //page defined later
+        <div id="rightdiv">
+            <button id="b1" onClick={clickFunction}>...</button>
+            <div id="options">
+                <h3><a href={linkDisabled ? undefined : "#"}>Sign in</a></h3> 
+                <h3><a href={linkDisabled ? undefined : "#"}>Booked shows</a></h3>
+                <h3><a href={linkDisabled ? undefined : "#"}>About us</a></h3>
+            </div>
+        </div>
+    )
+}
+
+export default About
