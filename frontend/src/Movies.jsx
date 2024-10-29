d4307cdedff52dd467ecb97158f0196cd6c743b5
Archit Rode <architvr15@gmail.com>
2024-10-29 11:22:12 +0530

    Added logo

--- frontend/src/Movies.jsx
+++ frontend/src/Movies.jsx
@@ -10,18 +10,18 @@ import movie6 from '../assets/movie6.jfif';
 function Movies() {
     return (
         <div className='homepage'>
-        <header className='container'>
-            <div className='top-bar'>
-                {/* <img src={logo} alt="Logo" className="logo" /> */}
-                <div className='search-container'>
-                    <input type="text" placeholder="Movie in mind?" className="search-bar" />
-                    <button className='search-button'>Search</button>
-                </div>
-                {/* <a href="/userprofile" className="login-link">
+            <header className='container'>
+                <div className='top-bar'>
+                    {/* <img src={logo} alt="Logo" className="logo" /> */}
+                    <div className='search-container'>
+                        <input type="text" placeholder="Movie in mind?" className="search-bar" />
+                        <button className='search-button'>Search</button>
+                    </div>
+                    {/* <a href="/userprofile" className="login-link">
                     <i className="fas fa-user"></i> Login
                 </a> */}
-            </div>
-            {/* <nav>
+                </div>
+                {/* <nav>
                 <div id='menu'>
                     <ul>
                         <li><a href="#home">Home</a></li>
@@ -32,31 +32,32 @@ function Movies() {
                     </ul>
                 </div>
             </nav> */}
-        </header>
+            </header>
 
-        {/* <h1>Welcome to MovieHub</h1> */}
-        <h2 id="mheading">Recommended Movies</h2>
+            {/* <h1>Welcome to MovieHub</h1> */}
+            <h2 id="mheading">Recommended Movies</h2>
 
-        <div className='recommended-movies'>
-            <div className='movie-item'>
-                <img src={movie6} alt='Movie 6' className='poster' id="m1" />
-                <p className='movie-name' id="md1">Monte Carlo</p>
+            <div className='recommended-movies'>
+                <div className='movie-item'>
+                    <img src={movie6} alt='Movie 6' className='poster' id="m1" />
+                    <p className='movie-name' id="md1">Monte Carlo</p>
+                </div>
+                <div className='movie-item'>
+                    <img src={movie2} alt='Movie 2' className='poster' id="m2" />
+                    <p className='movie-name' id="md2">Herbie:Fully Loaded</p>
+                </div>
+                <div className='movie-item'>
+                    <img src={movie3} alt='Movie 3' className='poster' id="m3" />
+                    <p className='movie-name' id="md3">Terminator: Judgement Day</p>
+                </div>
+                <div className='movie-item'>
+                    <img src={movie4} alt='Movie 4' className='poster' id="m4" />
+                    <p className='movie-name' id="md4">Zindagi Na Milegi Dobara</p>
+                </div>
+
             </div>
-            <div className='movie-item'>
-                <img src={movie2} alt='Movie 2' className='poster' id="m2"/>
-                <p className='movie-name' id="md2">Herbie:Fully Loaded</p>
-            </div>
-            <div className='movie-item'>
-                <img src={movie3} alt='Movie 3' className='poster' id="m3" />
-                <p className='movie-name' id="md3">Terminator: Judgement Day</p>
-            </div>
-            <div className='movie-item'>
-                <img src={movie4} alt='Movie 4' className='poster' id="m4"/>
-                <p className='movie-name' id="md4">Zindagi Na Milegi Dobara</p>
-            </div>
+            {/* <h2>Concerts</h2> */}
         </div>
-        {/* <h2>Concerts</h2> */}
-    </div>
     )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css'
 import RightButton from './rightButton'
 import Movies from './Movies'
+import logo from './assets/logo3.png'
 import ToggleTheme from './ToggleTheme'
 import Signup from './pages/signup';
 import Login from './pages/Login';
@@ -12,6 +13,9 @@ import Heading from './Heading'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToggleTheme />
+    <div className="logo-container">
+      <img src={logo} alt="TicketVerse Logo" className="logo-image" />
+    </div>
     <Router>
       <div class="contentdiv">
         <h1 id="heading">TicketVerse</h1>
@@ -61,19 +65,19 @@ createRoot(document.getElementById('root')).render(
             }
 
             <div id="about">
-            <h2>About us:</h2>
-            <p>Welcome to TicketVerse: your ultimate destination for seamless movie bookings and unforgettable cinematic experiences! At TicketVerse, we’re passionate about bringing the magic of movies to you with ease and convenience. Explore showtimes, book tickets, and secure your front-row seat to the latest blockbusters, all in just a few clicks. Join us and dive into a universe of entertainment, where every film journey begins with TicketVerse.</p>
-            <br></br>
-            <h3>Developers:</h3>
-            <h3 id="names">
-              <a id="names" href="#">Ayush Chakraborty</a>
+              <h2>About us:</h2>
+              <p>Welcome to TicketVerse: your ultimate destination for seamless movie bookings and unforgettable cinematic experiences! At TicketVerse, we’re passionate about bringing the magic of movies to you with ease and convenience. Explore showtimes, book tickets, and secure your front-row seat to the latest blockbusters, all in just a few clicks. Join us and dive into a universe of entertainment, where every film journey begins with TicketVerse.</p>
               <br></br>
-              <a id="names" href="#">Anupam G</a>
-              <br></br>
-              <a id="names" href="#">Archit Rode</a>
-              <br></br>
-            </h3>
-            </div> 
+              <h3>Developers:</h3>
+              <h3 id="names">
+                <a id="names" href="#">Ayush Chakraborty</a>
+                <br></br>
+                <a id="names" href="#">Anupam G</a>
+                <br></br>
+                <a id="names" href="#">Archit Rode</a>
+                <br></br>
+              </h3>
+            </div>
             {/* <br></br>
             <br></br>
             <br></br>
@@ -84,8 +88,8 @@ createRoot(document.getElementById('root')).render(
             <br></br> */}
           </>
         } />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       <RightButton />
     </Router>
