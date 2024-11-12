620d079ba39978fc2dfcca91649f33beedfc08da
Archit Rode <architvr15@gmail.com>
2024-11-12 10:20:26 +0530

    Modified signup page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,13 +14,13 @@ import MonteCarlo from './pages/MonteCarlo'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToggleTheme />
-    <div className="logo-container">
+    {/* <div className="logo-container">
       <img src={logo} alt="TicketVerse Logo" className="logo-image" />
-    </div>
+    </div> */}
     <Router>
-      {/* <div class="contentdiv">
-        <h1 id="heading">TicketVerse</h1>
-      </div> */}
+      <div className="logo-container">
+        <img src={logo} alt="TicketVerse Logo" className="logo-image" />
+      </div>
       <Heading />
       <div class="contentdiv1">
         <h2 id="desc">Your Front Row Seat to Every Adventure!</h2>
@@ -40,32 +40,6 @@ createRoot(document.getElementById('root')).render(
             <br></br>
             <br></br>
             <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            {
-
-            }
-
             <div id="about">
               <h2>About us:</h2>
               <p>Welcome to TicketVerse: your ultimate destination for seamless movie bookings and unforgettable cinematic experiences! At TicketVerse, we’re passionate about bringing the magic of movies to you with ease and convenience. Explore showtimes, book tickets, and secure your front-row seat to the latest blockbusters, all in just a few clicks. Join us and dive into a universe of entertainment, where every film journey begins with TicketVerse.</p>
@@ -80,14 +54,6 @@ createRoot(document.getElementById('root')).render(
                 <br></br>
               </h3>
             </div>
-            {/* <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br> */}
           </>
         } />
         <Route path="/monte-carlo" element={<MonteCarlo />} />
