f13c8c423e48861d5066f02d4b85bbe399629334
Archit Rode <architvr15@gmail.com>
2024-11-14 09:36:30 +0530

    Added settings button

--- frontend/src/components/rightButton.jsx
+++ frontend/src/components/rightButton.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import './rightButton.css'
 
 function RightButton() {
@@ -18,14 +18,16 @@ function RightButton() {
         setLinkDisabled(prev => !prev)
         setIsVisible(prev => !prev)
     }
- 
+
     return ( //the href thing is done so that once the fields are not visible, all their links 
         //become undefined effectively making it useless, and the fields do point to some other
         //page defined later
         <div id="rightdiv">
-            <button id="b1" onClick={clickFunction}>...</button>
-            <div id="options" style={{opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s ease', textAlign:'left', paddingRight: '15px'}}>
-                <h3><Link className="rightlink" to={linkDisabled ? undefined : "/login"}>Sign up/Log in</Link></h3> 
+            <button id="b1" onClick={clickFunction}>
+                <i className='fas fa-gear'></i>
+            </button>
+            <div id="options" style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s ease', textAlign: 'left', paddingRight: '15px' }}>
+                <h3><Link className="rightlink" to={linkDisabled ? undefined : "/login"}>Sign up/Log in</Link></h3>
                 <h3><Link className="rightlink" to={linkDisabled ? undefined : "#"}>Booked shows</Link></h3>
                 <h3><a className="rightlink" href={linkDisabled ? undefined : "/#about"}>About us</a></h3>
             </div>
