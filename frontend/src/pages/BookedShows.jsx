ba95e007797837d83e4012f40c5e64ed4fc7e6de
Anupam G <anupam2606@gmail.com>
2024-11-19 19:42:33 +0530

    Name update

--- frontend/src/pages/BookedShows.jsx
+++ frontend/src/pages/BookedShows.jsx
@@ -51,13 +51,14 @@ function BookedShows() {
     return (
         <div id="booked">
             <h2>Your Booked Shows</h2>
-            {firstName && <h3>Welcome, {firstName}!</h3>} {/* Display firstName */}
+            {/* {firstName && <h3>Welcome, {firstName}!</h3>} {/* Display firstName */} 
             {bookedShows.length === 0 ? (
                 <p>You have no bookings</p>
             ) : (
                 <div>
                     {bookedShows.map((booking, index) => (
                         <div key={index} className="booking-details">
+                            <p><strong>Movie Name : </strong> {booking.showtimeDetails.movieName} </p>
                             <p><strong>Showtime Id : </strong> {booking.showtimeId}</p>
                             <p><strong>Time : </strong> {booking.showtimeDetails.showTime} </p>
                             <p><strong>Seats : </strong> {booking.seats.join(', ')}</p>
