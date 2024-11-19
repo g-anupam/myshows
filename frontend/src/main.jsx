2b6cab018ee412ae6ae638701e32fa6520df8376
Anupam G <anupam2606@gmail.com>
2024-11-19 22:09:30 +0530

    css update for navbar buttons

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,7 +20,6 @@ import StripePayment from './pages/StripePayment';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <ToggleTheme />
       <FirstName />
       <header className="header-container">
         <div className="logo-container">
@@ -33,6 +32,7 @@ createRoot(document.getElementById('root')).render(
           </Link>
         </div>
         <RightButton />
+        <ToggleTheme />
       </header>
 
       <main className="content">
@@ -50,15 +50,11 @@ createRoot(document.getElementById('root')).render(
               <div id="about">
                 <h2>About us:</h2>
                 <p>Welcome to TicketVerse: your ultimate destination for seamless movie bookings and unforgettable cinematic experiences! At TicketVerse, we’re passionate about bringing the magic of movies to you with ease and convenience. Explore showtimes, book tickets, and secure your front-row seat to the latest blockbusters, all in just a few clicks. Join us and dive into a universe of entertainment, where every film journey begins with TicketVerse.</p>
-                <br />
                 <h3>Developers:</h3>
                 <h3 id="names">
                   <a id="names" href="https://github.com/AyushChakraborty">Ayush Chakraborty</a>
-                  <br />
                   <a id="names" href="https://github.com/g-anupam">Anupam G</a>
-                  <br />
                   <a id="names" href="https://github.com/Archit-Rode">Archit Rode</a>
-                  <br />
                 </h3>
               </div>
             </>
